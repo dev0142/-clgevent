a8b5ac7518202a223a599f39de3f782afd596bf5
Devashish Bhandari <devashishbhandari09@gmail.com>
2021-11-28 23:30:12 +0530

    try

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import '../App.css'
-import emoji from "react-easy-emoji";
-import photo from '../assests/Images/logo02.png'
 import Events from '../Pages/Events'
 import Contact from '../Pages/Contact'
 import image1 from "../assests/image1.jpeg"
 import image2 from "../assests/image2.jpeg"
 import image3 from "../assests/image3.jpeg"
-
-
 
 function Home() {
     return (
